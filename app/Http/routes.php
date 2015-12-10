236c9677e0cf7e16c2593124fbdba930dbef759f
Jeff Rodriguez <JeffR@tbpe.state.tx.us>
2015-12-10 16:07:18 -0600

    Just added a welcome controller, a view, and some routes. x

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,10 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', "WelcomeController@index");
+
+Route::get('contact', 'WelcomeController@contact');
